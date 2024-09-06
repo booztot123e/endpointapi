e095634020358da63da99f97b8ce444778eab707
booztot123e <tros2545@hotmail.com>
2024-09-06 16:04:56 +0700

    Initial commit

---
+++ sqlConfig.js
@@ -0,0 +1,34 @@
+var dbconfig = {
+    development: {
+        server: '10.21.43.203',
+        database:'team6_web_labDB',
+        user:'sa',
+        password:'P@ssw0rd',
+        port: 1433,
+        options:{
+            encript: true,
+            setTimeout: 12000,
+            enableArithAbort: true,
+            trustServerCertificate: true,
+            trustedconnection:  true,
+            instancename:  '10.21.43.203'  // SQL Server instance name
+        }
+    },
+    production: {
+        server: '10.21.43.203',
+        database:'team6_web_labDB',
+        user:'sa',
+        password:'P@ssw0rd',
+        port: 1433,
+        options:{
+            encript: true,
+            setTimeout: 12000,
+            enableArithAbort: true,
+            trustServerCertificate: true,
+            trustedconnection:  true,
+            instancename:  '10.21.43.203'  // SQL Server instance name
+        }
+    },
+
+};
+module.exports = dbconfig;
